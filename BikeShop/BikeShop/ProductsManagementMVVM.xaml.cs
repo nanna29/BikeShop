a506482ecf5fdf6f03a4acfb0db5d783fe6da151
Nellie <87843494+nanna29@users.noreply.github.com>
2023-04-27 13:36:47 +0900

    Add files via upload

--- BikeShop/BikeShop/ProductsManagementMVVM.xaml.cs
+++ BikeShop/BikeShop/ProductsManagementMVVM.xaml.cs
@@ -16,12 +16,11 @@ using System.Windows.Shapes;
 namespace BikeShop
 {
     /// <summary>
-    /// ProductsManagement.xaml에 대한 상호 작용 논리
+    /// ProductsManagementMVVM.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class ProductsManagement : Page
+    public partial class ProductsManagementMVVM : Page
     {
-        ProductsFactory factory = new ProductsFactory();
-        public ProductsManagement()
+        public ProductsManagementMVVM()
         {
             InitializeComponent();
         }
---
+++ BikeShop/BikeShop/ProductsManagementMVVMViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeShop
+{
+    public class ProductsManagementMVVMViewModel : Notifier
+    {
+        #region 입력과 출력 속성
+        private string searchInput;
+
+        public string SearchInput
+        {
+            get { return searchInput; }
+            set
+            {
+                searchInput = value;
+                base.OnPropertyChanged("SearchInput");
+                OnsearchInputChanged();
+            }
+        }
+
+        private IEnumerable<Product> foundProducts;
+
+        public IEnumerable<Product> FoundProducts
+        {
+            get { return foundProducts; }
+            set
+            {
+                foundProducts = value;
+                OnPropertyChanged("FoundProducts");
+            }
+        }
+
+        private Product selectedPrioduct;
+
+        public Product SelectedProduct
+        {
+            get { return selectedPrioduct; }
+            set
+            {
+                selectedPrioduct = value;
+                OnPropertyChanged("SelectedProduct");
+            }
+        }
+        #endregion
+
+        ProductsFactory factory = new ProductsFactory();
+
+        public ProductsManagementMVVMViewModel()
+        {
+            //선택적: 단순히 목록히 비어 있음을 확인
+            FoundProducts = Enumerable.Empty<Product>();
+        }
+
+        private void OnsearchInputChanged()
+        {
+            //선택적: 단순히 선택된 모든 제품이 비선택인지 확인
+            SelectedProduct = null;
+            FoundProducts = factory.FindProducts(SearchInput);
+        }
+    }
+}
